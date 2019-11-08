c0317ea30d3445d9655071c291503f9adb7f68e8
richard visser <Richard_Visser@outlook.com>
2019-11-08 16:25:18 +0100

    still not there

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -5,14 +5,26 @@ class CocktailsController < ApplicationController
 
   def show
     @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
   end
 
   def new
     @cocktail = Cocktail.new
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render 'new'
+    end
   end
 
   def create
-     @cocktail = Cocktail.new(params[:cocktail])
+    @cocktail = Cocktail.new(cocktail_params)
     @cocktail.save
+  end
+
+ private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
   end
 end
