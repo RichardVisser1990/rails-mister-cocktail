fdbb5aacaa033f708c14221187b02734d80073b9
richard visser <Richard_Visser@outlook.com>
2019-11-11 11:06:27 +0100

    unifnished business

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -7,8 +7,12 @@ class DosesController < ApplicationController
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
-    @dose.save
-    redirect_to cocktail_path(@cocktail)
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      # @review = Review.new
+      render "cocktails/show"
+    end
   end
 
   def destroy
